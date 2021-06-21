5dddc5655c0817a22e5562f91ba97f9b557d1c1c
shade <officialvinaysharma1098@gmail.com>
2021-06-21 17:52:46 +0530

    Basic Program of C added.

--- Average.c
+++ Average.c
@@ -1,12 +1,23 @@
-#include <stdio.h>
-#include <conio.h>
-void main()
+// WAP to calculate average of three numbers. 
+
+/* prompt for and read the three numbers
+   calculate average = sum of the numbers / 3.0
+   print average
+
+Field width as the name suggests is the width or space which needs to be given for that particular display line and 
+it is specified using numbers. %3.1f signifies field width is 3 and upto 1 decimal. */
+
+#include <stdio.h> 
+#include <conio.h> 
+void main() 
 {
-    int a,b,c;
-    double average;
-    printf("Enter the three numbers to calculate their average ");
-    scanf("%d%d%d",&a,&b,&c);
-    average=(a+b+c)/3.0;
-    printf("The average is %3.1f",average);
-    getch();
+    int a,b,c; //Variable decleration.
+    double average; 
+    printf("Enter the three numbers to calculate their average "); 
+    scanf("%d%d%d",&a,&b,&c); //reading  data given by user.
+    average=(a+b+c)/3.0; //calculating average and assigning to variable average.
+    printf("The average is %3.1f",average); //print value of average.
+    getch(); 
 }
+//scanf function is used to read the data from the screen .
+//%d are the frmat specifiers that reserves the memomory for the variables. eg. %d for integer (2 bit), %f for float (4 bit).
---
+++ Hello World.c
@@ -0,0 +1,23 @@
+//Wap to print hello world. 
+
+// In the following program we will print "Hello World". 
+#include <stdio.h>
+#include <conio.h>
+void main() 
+{
+   printf("Hello World");
+   getch();
+}
+/*void is the name of the function main. In our program we have to define one function as the main fuction.
+
+  studio.h is the library file used for printf and scanf functions . Liberary files are some standard files that are pre
+  defined in C containing many useful functions. 
+
+  conio.h is the library file used for getch function.
+
+  #include tells the compiler to include the following files.
+
+  printf function is used to print on the screen.
+
+  getch is used to get the output on the screen without this the program will pop and close. 
+*/
